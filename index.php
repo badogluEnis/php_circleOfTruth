46c5ca7dc4a115f53958fbdfefc616e559538835
PascalCornu <pascal.cornu@bluewin.ch>
2017-03-09 08:19:52 +0100

    added index.php and stylesheet

---
+++ index.php
@@ -0,0 +1,11 @@
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Circle of truth</title>
+  </head>
+  <body>
+
+  </body>
+</html>
