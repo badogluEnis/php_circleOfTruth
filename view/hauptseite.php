f86af572b62e99d08c798b52f455a7ad9b136a8b
badogluEnis <enis.badoglu.gerber@gmail.com>
2017-03-09 12:37:58 +0100

    added hauptseite, DefaulfControllerredirected

---
+++ view/hauptseite.php
@@ -0,0 +1,6 @@
+<div class="mittig_home">
+
+<button type="button" class="btn btn-lg btn-success">"Antwortmoeglichkeit 1"</button>
+<button type="button" class="btn btn-lg btn-danger">"Antwortmoeglichkeit 2"</button>
+
+</div>
