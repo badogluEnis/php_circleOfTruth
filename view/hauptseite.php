30de0f3209ebbed4f472961ba37bd4d5260dae72
badogluEnis <enis.badoglu.gerber@gmail.com>
2017-03-09 12:54:51 +0100

    added new css class / buttons on hauptseite renamed

--- view/hauptseite.php
+++ view/hauptseite.php
@@ -1,6 +1,6 @@
 <div class="mittig_home">
 
-<button type="button" class="btn btn-lg btn-success">"Antwortmoeglichkeit 1"</button>
-<button type="button" class="btn btn-lg btn-danger">"Antwortmoeglichkeit 2"</button>
+	<button type="button" class="btn btn-lg btn-success">Ja</button>
+	<button type="button" class="btn btn-lg btn-danger">Nein</button>
 
 </div>
