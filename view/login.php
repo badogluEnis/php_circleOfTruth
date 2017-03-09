82ed24ed8abc5f294e625c58ffe824ca86afeec2
badogluEnis <enis.badoglu.gerber@gmail.com>
2017-03-09 13:35:35 +0100

    integrated login.php

---
+++ view/login.php
@@ -0,0 +1,7 @@
+<div class="">
+
+<input type="text" placeholder="Benutzername" class="form-control">
+<input type="password" placeholder="Password" class="form-control">
+<button type="button" class="btn btn-lg btn-success">Anmelden</button>
+
+</div>
