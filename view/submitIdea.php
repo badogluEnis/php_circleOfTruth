397823db52a6d513065ce128bda41cfe79f443dc
PascalCornu <pascal.cornu@bluewin.ch>
2017-03-15 09:30:35 +0100

    added design for submit idea

--- view/submitIdea.php
+++ view/submitIdea.php
@@ -1,1 +1,14 @@
-<h3>Sende uns hier deine Ideen für neue Fragen zu!</h3>
+<h3>Sende uns hier deine Ideen für neue Fragen zu!</h3><br>
+<form id"sende">
+
+<textarea name ="frage" id ="frage" rows="5" cols="30" placeholder="Frage" style="resize:none" required></textarea><br><br>
+
+<textarea name ="antwkorr" id="antwkorr" rows="1" cols="25" placeholder="Korrekte Antowrt" style="resize:none" required></textarea>
+
+<textarea name ="antwfals" id ="antwfals" rows="1" cols="25" placeholder="Falsche Antowrt" style="resize:none" required></textarea><br><br>
+
+<label><input type="checkbox" id ="moral"> Die Frage ist eine Moralfrage*</label><br><br>
+
+<p>* Moralfrage bedeutet, dass es keine richtige Antwort gibt.</p>
+
+</form>
