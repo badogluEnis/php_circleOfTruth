33b44d1d093d246bf540901cb780680c952c6559
badogluEnis <enis.badoglu.gerber@gmail.com>
2017-03-15 09:55:10 +0100

    finished class submitIdea

--- view/submitIdea.php
+++ view/submitIdea.php
@@ -3,12 +3,14 @@
 
 <textarea name ="frage" id ="frage" rows="5" cols="30" placeholder="Frage" style="resize:none" required></textarea><br><br>
 
-<textarea name ="antwkorr" id="antwkorr" rows="1" cols="25" placeholder="Korrekte Antowrt" style="resize:none" required></textarea>
+<textarea name ="antwkorr" id="antwkorr" rows="1" cols="25" placeholder="Korrekte Antwort" style="resize:none" required></textarea>
 
 <textarea name ="antwfals" id ="antwfals" rows="1" cols="25" placeholder="Falsche Antowrt" style="resize:none" required></textarea><br><br>
 
 <label><input type="checkbox" id ="moral"> Die Frage ist eine Moralfrage*</label><br><br>
 
-<p>* Moralfrage bedeutet, dass es keine richtige Antwort gibt.</p>
+<p>* Moralfrage bedeutet, dass es keine richtige Antwort gibt. Trotzdem werden beide Antwortm&oumlglichkeiten ben&oumltigt</p> <br>
+
+<button type="submit" class="btn btn-success">Absenden</button>
 
 </form>
