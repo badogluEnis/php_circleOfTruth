dfa5b2bbfd6d0f6366eb7e3152f21faadf355b73
badogluEnis <enis.badoglu.gerber@gmail.com>
2017-03-16 10:17:11 +0100

    fixed unknown char in aboutUs.php

--- view/aboutUs.php
+++ view/aboutUs.php
@@ -25,7 +25,7 @@
    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, 
    <br>
-   Da wir keine Spenden annehmen, kannst du und helfen, indem du dich täglich einloggst, Fragen beantwortest und die Seite aktiv haltest.
+   Da wir keine Spenden annehmen, kannst du und helfen, indem du dich tÃ¤glich einloggst, Fragen beantwortest und die Seite aktiv haltest.
    Was uns auch sehr helfen w&uumlrde, ist wenn du unsere Webseite weiteremphiehlst oder deinen Freunden zeigst.
    So sehen wir das auch was von der Communtity zurw&uumlck kommt und erhalten so die Motivation die Seite immer mehr zu erweitern
 </p>
