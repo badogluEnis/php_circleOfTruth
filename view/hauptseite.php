4201ea013e615ba7aaceb6633237ba7edd3db81f
PascalCornu <pascal.cornu@bluewin.ch>
2017-03-24 14:49:22 +0100

    Now the "correct" answer is not always on the same side anymore.

--- view/hauptseite.php
+++ view/hauptseite.php
@@ -5,9 +5,15 @@
 	$min=0;
 	$max = count($fragen) - 1;
 	$index = mt_rand($min,$max);
-;
+
+	$fragenindex = mt_rand(0,1);
 
 
+	if ($fragenindex == 1) {
+		$fragenindex2 = 0;
+	} else {
+		$fragenindex2 = 1;
+	}
 
 	if (!empty($fragen)) {?>
 		<h1>Frage:</h1>
@@ -18,11 +24,8 @@
 	echo '<h2>Keine Fragen gefunden.</h2>';
 } ?>
 
-
-
-
-	<button type="button" class="btn btn-lg btn-success rundlinks"><?= $fragen[$index]['antworten'][1]->antwort; ?></button>
-	<button type="button" class="btn btn-lg btn-danger rundrechts"><?= $fragen[$index]['antworten'][0]->antwort; ?></button>
+	<button type="button" class="btn btn-lg btn-success rundlinks"><?= $fragen[$index]['antworten'][$fragenindex]->antwort; ?></button>
+	<button type="button" class="btn btn-lg btn-danger rundrechts"><?= $fragen[$index]['antworten'][$fragenindex2]->antwort; ?></button>
 
 
 
