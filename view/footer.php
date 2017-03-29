d29ea86a890801cc09c1558a152c58bc00a97acd
badogluEnis <enis.badoglu.gerber@gmail.com>
2017-03-29 09:54:18 +0200

    started to implement files and code for piechart

--- view/footer.php
+++ view/footer.php
@@ -9,7 +9,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
     <script src="js/password.js"></script>
-    <script src="js/piechart.js"></script>
+	<script type="text/javascript" src="/assets/script/canvasjs.min.js"></script> 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
   </body>
 </html>
--- view/recentlyAnswered.php
+++ view/recentlyAnswered.php
@@ -1,6 +1,5 @@
 <?php
 
-
 if (!empty($antworten)) { ?>
 	<table class="table table-hover mittig_admin">
 		<tr>
@@ -15,10 +14,46 @@ if (!empty($antworten)) { ?>
 				<td><?= $antwort->text; ?></td>
 				<td><?= $antwort->moralfrage ? '-' : $korrekt[$antwort->id - 1]->antwort; ?></td>
 				<td><?= $antwort->antwort; ?></td>
-				<td>Not Finished</td>
+				<td>
+				<div id="chartContainer" style="height: 50px; width: 100%;"></div>
+				</td>
 			</tr>
 		<?php } ?>
 		</table>
 		<?php } else {
 			echo '<h2>Keine kürzlich beantworteten Fragen.</h2>';
+		}?>
+
+		<script type="text/javascript">
+		window.onload = function () {
+			var chart = new CanvasJS.Chart("chartContainer",
+			{
+				title:{
+					text: ""
+				},
+				animationEnabled: true,
+				legend:{
+					verticalAlign: "center",
+					horizontalAlign: "left",
+					fontSize: 20,
+					fontFamily: "Helvetica"
+				},
+				theme: "theme2",
+				data: [
+				{
+					type: "pie",
+					indexLabelFontFamily: "Calibri",
+					indexLabelFontSize: 20,
+					indexLabel: "{label} {y}%",
+					startAngle:-20,
+					showInLegend: true,
+					dataPoints: [
+					{  y: 50%, label: "Richtig beantwortet" },
+					{  y: 50%, label: "Falsch beantwortet" }
+					]
+				}
+				]
+			});
+			chart.render();
 		}
+		</script>
